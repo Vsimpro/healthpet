3a31fdba986feb52a197d6741be1a75c92e60c64
laivii <91199307+laivii@users.noreply.github.com>
2023-11-11 23:39:03 +0200

    responsitivity and instructions

--- frontend/src/components/instructions/instructions.js
+++ frontend/src/components/instructions/instructions.js
@@ -4,11 +4,10 @@ export default function Instructions() {
     return (
         <div className="instructions">
             <h2>Instructions:</h2>
-            <p>The Center box of this page cointains our project. It is a simple Demo of a "Health pet" expansion for Huawei Healt -app. 
-                The idea behind it was to "guilt trip" young people to take better care of them selves. 
-                The state of the Cat depends on your personal health data like sleeping, steps and stress level. 
-                Depending on the date the Cat can appear sad, glad, stressed or absolutely joyful.<br></br><br></br> 
-                On the right side there is button with which you can change the dataset and see how the cat and statusbars change.</p>
+            <p>The middle box of this page cointains the visualization of our project. It is a simple Demo of a "Health pet" expansion for Huawei Health -app.</p> 
+            <p>The idea behind it was to "guilt trip" young people to take better care of themselves. Who would not be sad to see their "pet" not feel good?</p>
+            <p>The state of the Cat depends on your personal health data like sleeping, steps and stress level. Depending on the data, the Cat can appear sad, glad, stressed or absolutely joyful.</p>
+            <p>On the right side there is buttons with which you can change the dataset and see how the cat and status bars change.</p>
         </div>
     )
 }
