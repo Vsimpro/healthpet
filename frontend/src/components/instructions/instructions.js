9ee44a76448079a7d22b25fc5b851690df1c1dca
laivii <91199307+laivii@users.noreply.github.com>
2023-11-11 23:41:32 +0200

    instructions

--- frontend/src/components/instructions/instructions.js
+++ frontend/src/components/instructions/instructions.js
@@ -8,6 +8,7 @@ export default function Instructions() {
             <p>The idea behind it was to "guilt trip" young people to take better care of themselves. Who would not be sad to see their "pet" not feel good?</p>
             <p>The state of the Cat depends on your personal health data like sleeping, steps and stress level. Depending on the data, the Cat can appear sad, glad, stressed or absolutely joyful.</p>
             <p>On the right side there is buttons with which you can change the dataset and see how the cat and status bars change.</p>
+            <p>PS. the container is the size it would be on a phone. SO feel free to zoom a bit to see better. :D</p>
         </div>
     )
 }
